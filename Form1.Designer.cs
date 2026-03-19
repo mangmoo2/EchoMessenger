8d8ee582c0634ff0d7c51f3b587109825acb65b1
mangmoo2 <jihw0522@gmail.com>
2026-03-19 11:15:22 +0900

    과제4 코딩과테스트완료 컬렉션제어(Remove) 및시스템연동 1.선택항목삭제 2.전체초기화 3.글자수제한

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,6 +33,8 @@
             button1 = new Button();
             listBox1 = new ListBox();
             label2 = new Label();
+            button2 = new Button();
+            button3 = new Button();
             SuspendLayout();
             // 
             // label1
@@ -86,11 +88,36 @@
             label2.TabIndex = 4;
             label2.Text = "현재 대화:";
             // 
+            // button2
+            // 
+            button2.Font = new Font("맑은 고딕", 9F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button2.Location = new Point(625, 45);
+            button2.Name = "button2";
+            button2.Size = new Size(129, 23);
+            button2.TabIndex = 5;
+            button2.Text = "대화 기록 전체 삭제";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("맑은 고딕", 9F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button3.Location = new Point(520, 45);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 6;
+            button3.Text = "대화 삭제";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Info;
             ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(button2);
             Controls.Add(label2);
             Controls.Add(listBox1);
             Controls.Add(button1);
@@ -109,5 +136,7 @@
         private Button button1;
         private ListBox listBox1;
         private Label label2;
+        private Button button2;
+        private Button button3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -11,13 +11,20 @@ namespace Echo_Messenger
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)  
+        private void button1_Click(object sender, EventArgs e)
         {
             string typed_msg = textBox1.Text.Trim();   // text박스에 입력된 변수 담고 _ 앞뒤 공백 제거
+
+            if (typed_msg.Length > 50)          // 50자 이상 제한 
+            {
+                MessageBox.Show("메시지는 50자 이내로 입력할것");
+                return; // 실행을 중단 
+            }
+
             if (!string.IsNullOrWhiteSpace(textBox1.Text)) // 공백 검사  //과제2
             {
                 string time = DateTime.Now.ToString("[HH:mm:ss]"); // 메시지 앞에 현재시간 ([hh:mm:ss]) 을 자동으로 결합하여 리스트에 출력
-                
+
                 typed_msg = textBox1.Text;
                 listBox1.Items.Add($"{time} {typed_msg}"); // 리스트 박스의 아이템 목록에 추가하기 + time 시간 정보 출력
             }
@@ -38,7 +45,7 @@ namespace Echo_Messenger
 
         private void button1_KeyDown(object sender, KeyEventArgs e)
         {
-            
+
         }
 
         private void textBox1_KeyDown(object sender, KeyEventArgs e)
@@ -46,6 +53,25 @@ namespace Echo_Messenger
             if (e.KeyCode == Keys.Enter)     // enter키를 누르면   //과제2
             {
                 button1_Click(sender, e);  // 전송 버튼 클릭을 메서드로 호출 
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+                listBox1.Items.Clear();        // 리스트 박으안 아이템 전부 삭제 
+                label2.Text = $"현재 대화: 0개"; // 라벨 대화 개수 업데이트
+        }
+
+        private void button3_Click(object sender, EventArgs e)  // 대화 삭제 버튼
+        {
+            if (listBox1.SelectedIndex != -1)  // 
+            { 
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex); // 선택된 항목 삭제
+                label2.Text = $"현재 대화: {listBox1.Items.Count}개";// 라벨 대화 개수 업데이트
+            }
+            else // 예외 처리
+            { 
+                MessageBox.Show("삭제할 메시지를 선택"); // 삭제할 메세지 선택 경고 
             }
         }
     }
