cc26a85dfbbc24fc65119c742353d5993f7974fc
mangmoo2 <jihw0522@gmail.com>
2026-03-19 11:03:01 +0900

    과제2 코딩과테스트완료 커밋 제목 수정 과제2 완료후 "완료"란 단어 안적음

--- Form1.cs
+++ Form1.cs
@@ -14,15 +14,15 @@ namespace Echo_Messenger
         private void button1_Click(object sender, EventArgs e)  
         {
             string typed_msg = textBox1.Text.Trim();   // text박스에 입력된 변수 담고 _ 앞뒤 공백 제거
-            if (!string.IsNullOrWhiteSpace(textBox1.Text)) // 공백 검사 
+            if (!string.IsNullOrWhiteSpace(textBox1.Text)) // 공백 검사  //과제2
             {
                 string time = DateTime.Now.ToString("[HH:mm:ss]"); // 메시지 앞에 현재시간 ([hh:mm:ss]) 을 자동으로 결합하여 리스트에 출력
                 
                 typed_msg = textBox1.Text;
                 listBox1.Items.Add($"{time} {typed_msg}"); // 리스트 박스의 아이템 목록에 추가하기 + time 시간 정보 출력
             }
-            textBox1.Clear();  // 텍스트 박스 내용 삭제 
-            textBox1.Focus(); // 입력창에 입력 포커스 갖다 놓기
+            textBox1.Clear();  // 텍스트 박스 내용 삭제    //과제2
+            textBox1.Focus(); // 입력창에 입력 포커스 갖다 놓기   //과제2
             label2.Text = $"현재 대화: {listBox1.Items.Count}개"; //현재 리스트에쌓인 총메시지개수를 계산하여 하단 Label에 실시간으로업데이트
         }
 
@@ -43,7 +43,7 @@ namespace Echo_Messenger
 
         private void textBox1_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Enter)     // enter키를 누르면 
+            if (e.KeyCode == Keys.Enter)     // enter키를 누르면   //과제2
             {
                 button1_Click(sender, e);  // 전송 버튼 클릭을 메서드로 호출 
             }
