89b89cd9cdfe2868304216e8f6e98f697e51d353
mangmoo2 <jihw0522@gmail.com>
2026-03-19 10:21:01 +0900

    과제 1 코딩과 테스트 완료 list box를 중앙에 textbox를 list box 아래에 배치, label을 왼쪽위 폰트와 색깔을 수정, 눈에 띄게 보이게 함. 전송 버튼을 누르면 textbox에 입력한 내용이 listbox로 들어가 표시. 이후 textbox의 내용은 초기화 됨.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            listBox1 = new ListBox();
             SuspendLayout();
             // 
             // label1
@@ -36,17 +39,48 @@
             label1.AutoSize = true;
             label1.Font = new Font("바탕", 36F, FontStyle.Bold, GraphicsUnit.Point, 129);
             label1.ForeColor = SystemColors.HotTrack;
-            label1.Location = new Point(32, 30);
+            label1.Location = new Point(32, 20);
             label1.Name = "label1";
             label1.Size = new Size(417, 48);
             label1.TabIndex = 0;
             label1.Text = "Echo Messenger";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(42, 376);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(519, 23);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("맑은 고딕", 24F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button1.Location = new Point(591, 348);
+            button1.Name = "button1";
+            button1.Size = new Size(174, 76);
+            button1.TabIndex = 2;
+            button1.Text = "전송";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(32, 81);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(733, 244);
+            listBox1.TabIndex = 3;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(listBox1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
             Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
@@ -57,5 +91,8 @@
         #endregion
 
         private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private ListBox listBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,32 @@
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
 namespace Echo_Messenger
 {
     public partial class Form1 : Form
     {
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string typed_msg;  // text박스에 입력된 변수 담고
+            typed_msg = textBox1.Text;
+            listBox1.Items.Add(typed_msg); // 리스트 박스의 아이템 목록에 추가하기
+            textBox1.Clear();  // 텍스트 박스 내용 삭제 
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
